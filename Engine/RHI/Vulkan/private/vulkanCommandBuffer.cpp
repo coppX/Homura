c8746c1be9f974c0ef3cec65cf684b65e72690dd
coppx <fudaichao2133521@gmail.com>
2022-01-09 23:14:40 +0800

    add commandbuffer

---
+++ Engine/RHI/Vulkan/private/vulkanCommandBuffer.cpp
@@ -0,0 +1,43 @@
+//
+// Created by 最上川 on 2022/1/9.
+//
+
+#include <vulkanCommandBuffer.h>
+#include <vulkanDevice.h>
+#include <vulkanQueue.h>
+#include <debugUtils.h>
+
+namespace Homura
+{
+
+    void VulkanCommandBuffer::beginSingleTimeCommands()
+    {
+        VkCommandBufferAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        //allocInfo.commandPool = commandPool;
+        allocInfo.commandBufferCount = 1;
+        VERIFYVULKANRESULT(vkAllocateCommandBuffers(mDevice->getHandle(), &allocInfo, &mCommandBuffer));
+
+        VkCommandBufferBeginInfo beginInfo{};
+        beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+        VERIFYVULKANRESULT(vkBeginCommandBuffer(mCommandBuffer, &beginInfo));
+    }
+
+    void VulkanCommandBuffer::endSingleTimeCommands()
+    {
+        vkEndCommandBuffer(mCommandBuffer);
+
+        VkSubmitInfo submitInfo{};
+        submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+        submitInfo.commandBufferCount = 1;
+        submitInfo.pCommandBuffers = &mCommandBuffer;
+
+        vkQueueSubmit(mDevice->getGraphicsQueue(), 1, &submitInfo, VK_NULL_HANDLE);
+        vkQueueWaitIdle(mDevice->getGraphicsQueue());
+
+        //vkFreeCommandBuffers(mDevice->getHandle(), CommandPool, 1, mCommandBuffer);
+    }
+}
---
+++ Engine/RHI/Vulkan/public/vulkanCommandBuffer.h
@@ -0,0 +1,41 @@
+//
+// Created by 最上川 on 2022/1/9.
+//
+
+#ifndef HOMURA_VULKANCOMMANDBUFFER_H
+#define HOMURA_VULKANCOMMANDBUFFER_H
+#include <vulkan/vulkan.h>
+#include <memory>
+
+namespace Homura
+{
+    class VulkanDevice;
+
+    class VulkanCommandBuffer
+    {
+        VulkanCommandBuffer(std::shared_ptr<VulkanDevice> device)
+            : mCommandBuffer{VK_NULL_HANDLE}
+            , mDevice{device}
+        {
+
+        }
+
+        ~VulkanCommandBuffer()
+        {
+
+        }
+
+        VkCommandBuffer& getHandle()
+        {
+            return mCommandBuffer;
+        }
+
+        void beginSingleTimeCommands();
+
+        void endSingleTimeCommands();
+    private:
+        std::shared_ptr<VulkanDevice>   mDevice;
+        VkCommandBuffer     mCommandBuffer;
+    };
+}
+#endif //HOMURA_VULKANCOMMANDBUFFER_H
