8fe3acbba021def6591ac19371b0c50b4dd993a4
coppx <fudaichao2133521@gmail.com>
2022-01-14 09:55:52 +0800

    add Fence

---
+++ Engine/RHI/Vulkan/private/vulkanFence.cpp
@@ -0,0 +1,41 @@
+//
+// Created by FDC on 2022/1/14.
+//
+
+#include <vulkanFence.h>
+#include <vulkanDevice.h>
+#include <debugUtils.h>
+
+namespace Homura
+{
+    Fence::Fence(std::shared_ptr<VulkanDevice> device, bool signaled)
+        : mDevice{device}
+    {
+        VkFenceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+        createInfo.flags = signaled ? VK_FENCE_CREATE_SIGNALED_BIT : 0;
+
+        VERIFYVULKANRESULT(vkCreateFence(mDevice->getHandle(), &createInfo, nullptr, &mFence));
+    }
+
+    Fence::~Fence()
+    {
+        if (mFence != VK_NULL_HANDLE)
+        {
+            vkDestroyFence(mDevice->getHandle(), mFence, nullptr);
+        }
+    }
+
+    void Fence::reset()
+    {
+        if (mFence != VK_NULL_HANDLE)
+        {
+            vkResetFences(mDevice->getHandle(), 1, &mFence);
+        }
+    }
+
+    VkResult Fence::getResult()
+    {
+        return vkGetFenceStatus(mDevice->getHandle(), mFence);
+    }
+}
