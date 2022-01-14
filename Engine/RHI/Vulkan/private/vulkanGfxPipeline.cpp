bc4255833e0fa693707c9d5a3d77186af1fcf06f
coppx <fudaichao2133521@gmail.com>
2022-01-14 11:18:58 +0800

    add GfxPipeline declaration

---
+++ Engine/RHI/Vulkan/private/vulkanGfxPipeline.cpp
@@ -0,0 +1,35 @@
+//
+// Created by FDC on 2022/1/14.
+//
+
+#include <vulkanGfxPipeline.h>
+#include <vulkanRenderPass.h>
+#include <vulkanDevice.h>
+
+namespace Homura
+{
+    VulkanPipeline::VulkanPipeline(VulkanDevicePtr device, VulkanRenderPassPtr renderPass)
+        : mDevice{device}
+        , mRenderPass{renderPass}
+        , mVertexInputState{}
+        , mInputAssemblyState{}
+        , mTessellationState{}
+        , mViewportState{}
+        , mRasterizationState{}
+        , mMultisampleState{}
+        , mDepthStencilState{}
+        , mColorBlendState{}
+        , mPipeline{VK_NULL_HANDLE}
+        , mPipelineLayout{VK_NULL_HANDLE}
+        , mBlendAttachmentStates{}
+    {
+        mVertexInputState.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+        mInputAssemblyState.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+        mTessellationState.sType = VK_STRUCTURE_TYPE_PIPELINE_TESSELLATION_STATE_CREATE_INFO;
+        mViewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+        mRasterizationState.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+        mMultisampleState.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+        mDepthStencilState.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+        mColorBlendState.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    }
+}
---
+++ Engine/RHI/Vulkan/public/vulkanGfxPipeline.h
@@ -0,0 +1,43 @@
+//
+// Created by FDC on 2022/1/14.
+//
+
+#ifndef HOMURA_VULKANGFXPIPELINE_H
+#define HOMURA_VULKANGFXPIPELINE_H
+#include <vulkan/vulkan.h>
+#include <memory>
+#include <vector>
+
+namespace Homura
+{
+    class VulkanDevice;
+    class VulkanRenderPass;
+
+    class VulkanPipeline
+    {
+        using VulkanDevicePtr = std::shared_ptr<VulkanDevice>;
+        using VulkanRenderPassPtr = std::shared_ptr<VulkanRenderPass>;
+
+        VulkanPipeline(VulkanDevicePtr device, VulkanRenderPassPtr renderPass);
+        ~VulkanPipeline();
+
+    private:
+        VulkanDevicePtr                                     mDevice;
+        VulkanRenderPassPtr                                 mRenderPass;
+
+        VkPipelineVertexInputStateCreateInfo                mVertexInputState;
+        VkPipelineInputAssemblyStateCreateInfo              mInputAssemblyState;
+        VkPipelineTessellationStateCreateInfo               mTessellationState;
+        VkPipelineViewportStateCreateInfo                   mViewportState;
+        VkPipelineRasterizationStateCreateInfo              mRasterizationState;
+        VkPipelineMultisampleStateCreateInfo                mMultisampleState;
+        VkPipelineDepthStencilStateCreateInfo               mDepthStencilState;
+        VkPipelineColorBlendStateCreateInfo                 mColorBlendState;
+
+        VkPipeline                                          mPipeline;
+        VkPipelineLayout                                    mPipelineLayout;
+
+        std::vector<VkPipelineColorBlendAttachmentState>    mBlendAttachmentStates{};
+    };
+}
+#endif //HOMURA_VULKANGFXPIPELINE_H
