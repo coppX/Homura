0b6dd4e3b21443ca7ff59ecf319bdac5c9c09ec9
coppx <fudaichao2133521@gmail.com>
2022-08-02 21:53:34 +0800

    fix can't fill data into vertex buffer and index buffer

--- engine/rhi/vulkan/private/vulkanBuffer.cpp
+++ engine/rhi/vulkan/private/vulkanBuffer.cpp
@@ -94,6 +94,7 @@ namespace Homura
     void VulkanBuffer::updateBufferByStaging(void *pData, uint32_t size)
     {
         mStagingBuffer = new VulkanBuffer(mDevice, mCommandBuffer, size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+        mStagingBuffer->fillBuffer(pData, size);
         copyBuffer(*mStagingBuffer, *this, static_cast<VkDeviceSize>(size));
     }
 
