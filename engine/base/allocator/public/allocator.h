f3443bf1e260eb61812d57bcfc4683bf03f804e5
coppx <fudaichao2133521@gmail.com>
2022-08-11 22:59:10 +0800

    add allocator dir

---
+++ engine/base/allocator/public/allocator.h
@@ -0,0 +1,51 @@
+//
+// Created by Administrator on 2022/8/11/011.
+//
+
+#ifndef HOMURA_ALLOCATOR_H
+#define HOMURA_ALLOCATOR_H
+#include <cassert>
+
+#if defined(WIN32)
+#include <malloc.h>
+#endif
+namespace Base
+{
+    inline void* aligned_alloc(size_t size, size_t align) noexcept
+    {
+        // 'align' must be a power of two and a multiple of sizeof(void*)
+        align = (align < sizeof(void*)) ? sizeof(void*) : align;
+        assert(align && !(align & align - 1));
+        assert((align % sizeof(void*)) == 0);
+        void* p = nullptr;
+
+        // must be a power of two and >= sizeof(void*)
+        while (align < sizeof(void*))
+        {
+            align <<= 1;
+        }
+#if defined(WIN32)
+        p = ::_aligned_malloc(size, align);
+#else
+        ::posix_memalign(&p, align, size);
+#endif
+        return p;
+    }
+
+    inline void aligned_free(void* p)
+    {
+#if defined(WIN32)
+        ::_aligned_free(p);
+#else
+        ::free(p);
+#endif
+    }
+
+    template<typename TYPE>
+    class allocator
+    {
+        
+    };
+}
+
+#endif //HOMURA_ALLOCATOR_H
